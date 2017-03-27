6142cbbc076a96999b1038b6a7239326eea51126
lauradacol <laura.dacol@gmail.com>
2017-03-27 11:27:36 -0300

    Add files via upload

---
+++ Exemplo2.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Exemplo2 {
+	public static void main(String[] args){
+		Scanner entrada = new Scanner(System.in);
+/*		
+		int x;
+		System.out.println("Informe o valor de x:");
+		x = entrada.nextInt();
+		
+		System.out.println("O valor de x é: " + x);
+		
+		char resposta;
+		System.out.println("Vai ir na festa? (s/n)");
+		resposta = entrada.nextLine().charAt(0);
+		System.out.println(resposta);
+	*/
+	
+	int n;
+	char s;
+	
+	System.out.printf("Informe um Número Inteiro: ");
+	n = entrada.nextInt();
+	
+	entrada.nextLine();
+	
+	System.out.printf("Informe uma cadeia de caracteres\n");
+	s = entrada.nextLine();
+				
+	}
+}
