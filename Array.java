b47985d42fc5e65272df5054408376f6b9c41a60
lauradacol <laura.dacol@gmail.com>
2017-03-28 14:12:51 -0300

    Add files via upload

---
+++ Array.java
@@ -0,0 +1,22 @@
+class Array{
+	public static void main(String[] args){
+		Scanner entrada = new Scanner(System.in);
+
+		tamanho_array = entrada.nextInt();
+		int [] meuArray; 
+
+		for(int i = 0; i< tamanho_array; i = i+1){
+			meuArray[i] = entrada.nextInt();
+
+		}
+
+		print(meuArray);
+
+
+
+
+		
+
+		}
+	
+	}
