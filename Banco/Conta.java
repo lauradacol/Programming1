2cf550fb61b6d55e23c077d9714bc44861796399
lauradacol <laura.dacol@gmail.com>
2017-04-02 22:30:57 -0300

    Update Conta.java

--- Banco/Conta.java
+++ Banco/Conta.java
@@ -2,33 +2,41 @@ class Conta{
 	int numero;
 	String dono;
 	double saldo;	
+
+	public void printOperacao(){
+		System.out.println("Operação realizada com sucesso!");
+		}
 		
-	public double consulta_saldo(){
-		return this.saldo;		
+	public void consultaSaldo(){
+		System.out.println("Saldo: " + this.saldo);
 		}
 	
-	public boolean sacar(double valorSaque){
-			
+	public void sacar(double valorSaque){
 		if(valorSaque<0){
-			return false;
+			System.out.println("Saldo insuficiente");
 			}	
 					
 		else if(this.saldo>=valorSaque){			
 			this.saldo-=valorSaque;
-			return true;						
+			printOperacao();
+			consultaSaldo();
 		}
-		else{
-			return false;
-			}	
-		}
-	
-	public double deposito(double valorDeposito){
-		this.saldo+=valorDeposito;
-		return this.saldo;	
 		
+		else{
+			System.out.println("Saldo insuficiente para saque");
+			}
 		}
 	
+	public void deposito(double valorDeposito){
+		this.saldo+=valorDeposito;
+		consultaSaldo();
+		}
 	
+	public void transferencia(Conta contaDestino, double valorTransferencia){
+		sacar(valorTransferencia);
+		contaDestino.saldo+=valorTransferencia;
+		printOperacao();
+		}
 	}
 
 
