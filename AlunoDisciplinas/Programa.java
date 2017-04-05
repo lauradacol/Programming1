4f772b222150199af3c5d6bbeb23b7280337aa78
lauradacol <laura.dacol@gmail.com>
2017-04-05 09:33:43 -0300

    Update Programa.java

--- AlunoDisciplinas/Programa.java
+++ AlunoDisciplinas/Programa.java
@@ -1,40 +1,24 @@
 class Programa{
     public static void main(String[] args){
-        Aluno aa = new Aluno();
-        aa.nome="Pri";
-        aa.matricula=2535;
-        
+        Aluno aa = new Aluno(2535, "Pri");
         System.out.println(aa);
         
-        Disciplina d1=new Disciplina();
-        d1.creditos=4;
-        d1.codigo=123456;
-        d1.nome="Programação I";
-        
+        Disciplina d1=new Disciplina(4, 123456, "Programação I");
         aa.matricular(d1);
         
-        d1=new Disciplina();
-        d1.nome="ED II";
-        d1.codigo=12547;
-        d1.creditos=4;
+        d1=new Disciplina(4, 12547, "ED II");
         aa.matricular(d1);
         
-        Disciplina d2=new Disciplina();
-        d2.nome="Algebra Linear";
-        d2.codigo=12548;
-        d2.creditos=2;
+        Disciplina d2=new Disciplina(2, 12548, "Álgebra Linear");
         aa.matricular(d2);
         
-        d2=new Disciplina();
-        d2.nome = "Cálculo I";
-        d2.codigo = 12349;
-        d2.creditos=6;
+        d2=new Disciplina(6, 12349, "Cálculo I");
         aa.matricular(d2);
         
         aa.listarDisciplinas();
         System.out.println(aa);
-        aa.disciplinas[0].encerrar();
         
+        aa.disciplinas[0].encerrar();
         aa.listarDisciplinas();
         
         
