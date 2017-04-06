4eb956262faa40e78b2c7aad124c9972764383a2
Laura Helena Bento Dacol <laura@localhost.localdomain>
2017-04-06 19:02:05 -0300

    tinha faltado adicionar Data.java

---
+++ Medicamentos/Data.java
@@ -0,0 +1,18 @@
+class Data{
+	int dia;
+	int mes;
+	int ano;
+	
+	public Data(int dia, int mes, int ano){
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
+		}
+	
+	 public String toString(){
+        return this.dia + "/" + this.mes + "/" + this.ano;
+    }
+		
+
+}
+
