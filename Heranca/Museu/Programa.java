10551c3b2482bed39252be9d6bacd5377197fbc7
lauradacol <laura.dacol@gmail.com>
2017-05-03 09:10:47 -0300

    Update Programa.java

--- Heranca/Museu/Programa.java
+++ Heranca/Museu/Programa.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+
 public class Programa {
 	public static void main(String[] args) {
+
+		ArrayList<Animal> museu = new ArrayList<Animal>();
+		
+		museu.add(new Mamifero("Mamífero",150, 4,"Amarelo","Terra",2.0,"Camelo","", ""));
+		museu.add(new Peixe("Peixe", 300, 0, "Cinzento", "Mar", 1.5, "Tubarão", "Barbatanas e Cauda"));
+		museu.add(new Mamifero("Mamífero", 180, 4, "Vermelho","Terra", 0.5, "Uso do Canadá", "" ,"Mel"));
+		
+		for(Animal a : museu){
+			a.setDados();
+			}
+		
+		
+
+/*
 		Mamifero camelo = new Mamifero("Mamífero",150, 4,"Amarelo","Terra",2.0,"Camelo","", "");
 		Peixe tubarao = new Peixe("Peixe", 300, 0, "Cinzento", "Mar", 1.5, "Tubarão", "Barbatanas e Cauda");
 		Mamifero ursoDoCanada = new Mamifero("Mamífero", 180, 4, "Vermelho","Terra", 0.5, "Uso do Canadá", "" ,"Mel");
@@ -10,7 +26,7 @@ public class Programa {
 		
 		tubarao.setCaracteristica("Carnivoro");
 		tubarao.getDadosPeixe();
-		
+	*/	
 	}
 
 }
