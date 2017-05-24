f65fb9a868c2b5be320c180aede13587501e063b
aluno-uffs <laura.dacol@gmail.com>
2017-05-24 09:04:53 -0300

    add polimorfismo

--- Polimorfismo/PaiFilha/Programa.java
+++ Polimorfismo/PaiFilha/Programa.java
@@ -1,15 +1,15 @@
 class Programa{
 	public static void main(String[] args){
 		
-		Pai p = new Pai(10);
-		Pai f = new Filha(30,5);
+		Pai p = new Filha(30,5);
 		
 		p.m1();
 		p.m2();
 		
-		f.m1();
-		f.m2();
-		f.m3();
+		Filha f = (Filha) p;
+		p.m1();
+		p.m2();
+		p.m3();
 		
 		}
 		
